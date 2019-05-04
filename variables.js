4e34418ccc137e229a481f95e9e284b45d5951b2
shashankchinks <shashankchinks@gmail.com>
2019-05-04 12:43:03 +0530

    code are commented fully

---
+++ variables.js
@@ -0,0 +1,6 @@
+var num3 = 100;
+{
+    var num2 = 200;
+    var result = num3 + num2;
+}
+console.log(result);
---
+++ variables.ts
@@ -0,0 +1,10 @@
+// let num3 = 100;
+// {
+//     let num2 = 200;
+//     let result = num3 + num2;
+// }
+
+
+// console.log(result);
+
+// because let is not allowing to access outside of the scope.

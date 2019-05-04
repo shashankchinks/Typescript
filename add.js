03c772d5f2ee0eb39450df7d6a9db8439e5c1faa
shashankchinks <shashankchinks@gmail.com>
2019-05-04 12:17:57 +0530

    function for adding two numbers

---
+++ add.js
@@ -0,0 +1,6 @@
+function add(num1, num2) {
+    var sum = num1 + num2;
+    return sum;
+}
+var output = add(10, 20);
+console.log(output);
---
+++ add.ts
@@ -0,0 +1,7 @@
+function add(num1, num2){
+    var sum = num1 + num2;
+    return sum;
+}
+
+var output = add(10,20);
+console.log(output);
